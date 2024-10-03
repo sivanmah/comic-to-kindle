8d31efa08925b36415951c38cae026c06a8e1d19
sivanmah <sivanmahmud@gmail.com>
2024-10-03 11:49:12 +0200

    fixed header

--- my-comic-converter/src/App.tsx
+++ my-comic-converter/src/App.tsx
@@ -113,7 +113,7 @@ function App() {
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center">
       <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
         <h1 className="text-3xl font-bold mb-6 text-center text-blue-600">
-          Kindle2Comic
+          Comic2Kindle
         </h1>
         <form
           onSubmit={handleSubmit}
